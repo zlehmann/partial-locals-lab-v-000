905c4764adaa3138066910df19fd496d9ca6a8cf
Zachary Lehmann <zlehmann@gmail.com>
2019-01-30 02:08:41 +0000

    Automatically backed up by Learn

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,7 +1,7 @@
 class ClassroomsController < ApplicationController
   def show
     @classroom = Classroom.find(params[:id])
-    @student = @classroom.oldest_student
+    @student = @classroom.students
   end
 
   def index
